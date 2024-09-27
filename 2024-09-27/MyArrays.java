95e174d68325eedf4de89a20247c2bae3d745599
anjali814 <abechu60@stuy.edu>
2024-09-27 10:52:59 -0400

    working on test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,5 +1,11 @@
 public class MyArrays {
 public static String arrayToString (int [] array)
 {return Array.toString (array);}
-public static void main (String args [])
+
+
+public static void main (String [] args)
+{int [] original = {1 ,2 ,3, 4}
+int [] copy = returnCopy(original);
+
+System.out.println}
 }
