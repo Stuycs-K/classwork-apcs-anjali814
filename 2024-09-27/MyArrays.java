0ecf3c6ded18c7e163eb325d8df7dbcbd6ff5732
anjali814 <abechu60@stuy.edu>
2024-09-27 10:59:10 -0400

    recommitting because I don't think it committed right before

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -3,9 +3,17 @@ public static String arrayToString (int [] array)
 {return Array.toString (array);}
 
 
+
+
+
+
+
+
+
 public static void main (String [] args)
 {int [] original = {1 ,2 ,3, 4}
 int [] copy = returnCopy(original);
 
-System.out.println}
+System.out.println ("original" + arrayToString (original));
+}
 }
