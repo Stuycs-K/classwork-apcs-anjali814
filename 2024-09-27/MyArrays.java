4e645b2cd137a2e921c4aa10e553074e58dde359
anjali814 <abechu60@stuy.edu>
2024-09-28 06:03:10 -0400

    testing array to string

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,19 +1,24 @@
 public class MyArrays {
-public static String arrayToString (int [] array)
-{return Array.toString (array);}
-
-
-
-
-
-
-
+  public static String arrayToString(int[] array) {
+    String result = "[";
+    for (int i = 0; i < array.length; i++) {
+        result += array[i];
+        if (i < array.length - 1) {
+            result += ", ";
+        }
+    }
+    result += "]";
+    return result;
+}
 
 
 public static void main (String [] args)
-{int [] original = {1 ,2 ,3, 4}
-int [] copy = returnCopy(original);
+{int[] testArray1 = {1, 2, 3, 4, 5};
+  System.out.println("Test Case 1: " + arrayToString(testArray1)); // Expected: [1, 2, 3, 4, 5]
 
-System.out.println ("original" + arrayToString (original));
+//int [] original = {1 ,2 ,3, 4}
+//int [] copy = returnCopy(original);
+
+//System.out.println ("original" + arrayToString (original));
 }
 }
