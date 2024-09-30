fcddc29f347e50e11cc423bc144a13af2d4ee7e9
anjali814 <abechu60@stuy.edu>
2024-09-29 20:50:54 -0400

    copy fucntion written

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -11,10 +11,33 @@ public class MyArrays {
     return result;
 }
 
+public static int[] returnCopy(int[] ary) {
+  int[] copyOfArray = new int[ary.length];
+  for (int i = 0; i < ary.length; i++) {
+    copyOfArray[i] = ary[i];
+  }
+  return copyOfArray;
+}
+
+
+
+
+
+
+
 
 public static void main (String [] args)
+// test for array to string; passed
 {int[] testArray1 = {1, 2, 3, 4, 5};
   System.out.println("Test Case 1: " + arrayToString(testArray1)); // Expected: [1, 2, 3, 4, 5]
+
+
+
+
+
+
+
+
 
 //int [] original = {1 ,2 ,3, 4}
 //int [] copy = returnCopy(original);
