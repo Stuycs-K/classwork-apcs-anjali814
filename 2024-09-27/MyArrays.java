2e46d38ff5354f2b6a3c264e68ce2a62db3c7d17
anjali814 <abechu60@stuy.edu>
2024-09-29 21:25:23 -0400

    concat function added

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -19,6 +19,13 @@ public static int[] returnCopy(int[] ary) {
   return copyOfArray;
 }
 
+public static int[] concatArray(int[]ary1,int[]ary2) {
+  int[] result = new int[ary1.length + ary2.length];
+  for (int i = 0; i < ary1.length; i++) {
+    result[i] = ary1[i];
+  }
+  return result;
+}
 
 
 
@@ -27,11 +34,17 @@ public static void main (String [] args){
 int[] testArray1 = {1, 2, 3, 4, 5};
   System.out.println("Test Case 1: " + arrayToString(testArray1)); // Expected: [1, 2, 3, 4, 5]
 
-
-int [] original = {1 ,2 ,3, 4};
+// test for copy; passed
+int [] original = {1, 2, 3, 4};
 int [] copy = returnCopy(original);
-System.out.println ("original" + arrayToString (original)); // Expected: [1, 2, 3, 4]
+System.out.println ("Original: " + arrayToString (original)); // Expected: [1, 2, 3, 4]
 System.out.println("Copy: " + arrayToString(copy)); // Expected: [1, 2, 3, 4]
 System.out.println("Arrays are equal: " + arrayToString(original).equals(arrayToString(copy))); // Expected: true
+
+// test for concat;
+int [] array1 = {1, 2, 3, 4};
+int [] array2 = {5, 6, 7, 8};
+int[] concatenated = concatArray(array1, array2);
+System.out.println("Concatenated: " + arrayToString(concatenated)); // Expected: [1, 2, 3, 4, 5, 6, 7, 8]
 }
 }
