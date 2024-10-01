18c5ce91c15f0f156e3ca40eb970d5dbb67d6675
anjali814 <abechu60@stuy.edu>
2024-09-30 23:44:36 -0400

    add file

---
+++ 2024-09-30/ArrayMethods.java
@@ -0,0 +1,45 @@
+//anjali bechu
+//abechu60@stuy.edu
+
+//jaden bae
+//jabae50@stuy.edu
+
+public class ArrayMethods{
+  public static String arrayToString(int ary[]){
+    String combined = "[";
+    for (int i = 0; i < ary.length; i++){
+      if (i == ary.length - 1){
+        combined += ary[i];
+      }else{
+        combined += ary[i] + ", ";
+      }
+    }
+    combined += "]";
+    return combined;
+    }
+
+public static String arrToString(int[][]ary){
+  if (ary.length == 0) return "[]";
+  String combined = "[";
+  for (int i = 0; i < ary.length; i++){
+    combined += arrayToString(ary[i]);
+    if (i < ary.length - 1) {
+      combined += ", ";
+    }
+  }
+  combined += "]";
+  return combined;
+}
+
+public static void main(String[] args){
+  int[][] arr = {
+    {2,3,4},
+    {5,6,7},
+    {8,0,4}
+  };
+  System.out.println(arrToString(arr)); // expected: "[[2, 3, 4], [5, 6, 7], [8, 0, 4]"
+
+    }
+
+
+}
