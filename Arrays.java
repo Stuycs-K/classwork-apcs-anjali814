e093ca6140cce556a87a93b13af4e0cf83e1cb76
anjali814 <abechu60@stuy.edu>
2024-10-07 18:59:10 -0400

    Add Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+public class ArrayTest {}
+public static void main(String[]args){
+  int [] anArray = new int [i]{
+    if (i = 100; i <= 11; i++)
+    {i += 1}
+  }
+
+  //1 initialize the array
+  //int[] anArray = ???
+/// directions for classwork
+/// Initialize an array of 11 elements.
+///  Print the variable.
+/// Create a for loop that will initialize this array with 100 through 200 (increments of 10).
+/// print the first last and middle values.
+// Use a for loop to print each value of the array variable separately (use System.out.print instead of println)
+
+  //2 print the array variable
+  System.out.print("part 2: ");
+
+  //Part 3 Write the loop to initialize it
+
+
+  //Part 4 anArray[0], anArray[5],anArray[10] to see if it worked
+  System.out.print("part 4: ");
+
+  //Part 5 Write a loop to print the whole array (on one line)
+  System.out.print("part 4: ");
+}
+}
