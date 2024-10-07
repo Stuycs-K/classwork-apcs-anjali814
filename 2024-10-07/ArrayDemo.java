b0626ee3ddce6970f937202cf651f02423814c89
anjali814 <abechu60@stuy.edu>
2024-10-07 10:49:30 -0400

    adding in my old methods

---
+++ 2024-10-07/ArrayDemo.java
@@ -0,0 +1,83 @@
+public class ArrayDemo {
+
+public static void main(String[]args){
+    //write your tests here!
+    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
+    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
+    //do not use any other Arrays.method()
+
+  }
+
+public static String arrToString(int ary[]){
+    String combined = "[";
+    for (int i = 0; i < ary.length; i++){
+      if (i == ary.length - 1){
+        combined += ary[i];
+      }else{
+        combined += ary[i] + ", ";
+      }
+    }
+    combined += "]";
+    return combined;
+    }
+
+public static String arrToString(int[][]ary){
+  if (ary.length == 0) return "[]";
+  String combined = "[";
+  for (int i = 0; i < ary.length; i++){
+    combined += arrToString(ary[i]);
+    if (i < ary.length - 1) {
+      combined += ", ";
+    }
+  }
+  combined += "]";
+  return combined;
+}
+
+public static int arr2DSum(int[][]nums){
+  int sum = 0;
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = 0; j < nums[i].length; j++) {
+      sum = sum + nums [i][j];
+    }
+  }
+  return sum;
+}
+
+public static void replaceNegative(int[][] vals){
+  for (int i = 0; i < vals.length; i++) {    // row
+    for (int c = 0; c < vals[i].length; c++) {    //column
+      if (vals[i][c] < 0) {
+        if (i == c) {
+          vals[i][c] = 1;
+        }
+        else {
+          vals[i][c] = 0;
+        }
+      }
+    }
+  }
+}
+
+public static int[][] copy(int[][] nums){
+  int[][] copied = new int[nums.length][];
+  for (int i = 0; i < nums.length; i++) {
+    copied[i] = new int[nums[i].length];
+    for (int j = 0; j < nums[i].length; j++) {
+      copied[i][j] = nums[i][j];
+    }
+  }
+  return copied;
+}
+
+public static int[][] swapRC(int[][]nums){
+  int[][] result = new int[nums[0].length][nums.length];
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = 0; j < nums[i].length; j++) {
+      result[j][i] = nums[i][j];
+    }
+  }
+  return result;
+}
+
+}
