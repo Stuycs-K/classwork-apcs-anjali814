4aea14e2f8c85ffe9d181bf121ad4d0a9151d59b
anjali814 <abechu60@stuy.edu>
2024-11-04 10:55:23 -0500

    changing from an arraylist to an array

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,17 +1,27 @@
 public class TriangleTester {
 
+/*
   public static int countTrianglesA(String filename) {
+    int count = 0
     //do file parsing
-    return 0; //so it can compile
-  }
 
-  public static boolean isTriangle(ArrayList<Integer> sides) {
+
+    return count; //so it can compile
+  }
+  */
+
+  public static boolean isTriangle(int[] sides) {
     boolean isTri = true;
-    for (int i = 0; i < sizes.size() && isTri; i++) {
-      if (sizes.get(i) + sides.get(i + 1) <= sides.get(i + 2)) {
+    for (int i = 0; i < size.length && isTri; i++) {
+      if (size[0] + size[1] >= size[2]) {
         isTri = false;
       }
     }
     return isTri;
   }
+
+  public static void main(String[] args){
+    System.out.println()
+      }
+
 }
