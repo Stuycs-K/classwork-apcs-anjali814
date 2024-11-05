97da9f8c7d50deadb0c11056869ed5174e6931a4
anjali814 <abechu60@stuy.edu>
2024-11-05 17:34:56 -0500

    added the countTriangles code

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,15 +1,27 @@
 public class TriangleTester {
 
-/*
   public static int countTrianglesA(String filename) {
     int count = 0
-    //do file parsing
+    try {
+      File file = new File(filename);
+      while(file.hasNextLine()){
+        system.out.println(input.next());
+        String[] parts = line.split(" ");
+        if (parts.length == 3) {
+          int a = Integer.parseInt(parts[0]);
+          int b = Integer.parseInt(parts[1]);
+          int c = Integer.parseInt(parts[2]);
+          if (isTriangle(a, b, c)) {
+            count++;
+          }
+        }
+      }
+    }
 
-
-    return count; //so it can compile
+    return count;
   }
-  */
-
+  
+/*
   public static boolean isTriangle(int[] sides) {
     boolean isTri = true;
     for (int i = 0; i < size.length && isTri; i++) {
@@ -19,6 +31,7 @@ public class TriangleTester {
     }
     return isTri;
   }
+  */
 
   public static void main(String[] args){
     System.out.println()
