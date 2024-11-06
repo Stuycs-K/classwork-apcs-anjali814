360dfc031970128740b2d335620242a3f8ac43db
anjali814 <abechu60@stuy.edu>
2024-11-05 22:52:58 -0500

    I changed the code to use arraylists but I dont think it was workign properly so i edited my istriangle boolean and changed majority of part b to work wiht the new istriangle since it was no longer an array and now it works after almost losing my mind

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -23,7 +23,7 @@ public class TriangleTester {
           int b = Integer.parseInt(parts[1]);
           int c = Integer.parseInt(parts[2]);
 
-          if (isTriangle(new int[]{a, b, c})) {
+          if (isTriangle(a, b, c)) {
             count++;
           }
         }
@@ -36,47 +36,41 @@ public class TriangleTester {
     return count;
   }
 
-  public static boolean isTriangle(int[] sides) {
-    return sides[0] + sides[1] > sides[2] &&
-    sides[0] + sides[2] > sides[1] &&
-    sides[1] + sides[2] > sides[0];
+  public static boolean isTriangle(int a, int b, int c) {
+    return (a + b > c) && (a + c > b) && (b + c > a);
   }
 
 
 // Part B
    public static int countTrianglesB(String filename) {
      int count = 0;
-     int[] column1 = new int[0];
-     int[] column2 = new int[0];
-     int[] column3 = new int[0];
 
      try {
        File file = new File(filename);
        Scanner input = new Scanner(file);
 
+       String[] lines = new String[3];
+       int lineIndex = 0;
+
        while(input.hasNextLine()){
-         String line = input.nextLine();
-         String[] parts = line.split("\\s+");
+         lines[lineIndex] = input.nextLine();
+         lineIndex++;
 
-         if (parts.length == 3) {
-           int a = Integer.parseInt(parts[0]);
-           int b = Integer.parseInt(parts[1]);
-           int c = Integer.parseInt(parts[2]);
+         if (lineIndex == 3) {
+           String[] parts1 = lines[0].split("\\s+");
+           String[] parts2 = lines[1].split("\\s+");
+           String[] parts3 = lines[2].split("\\s+");
 
-           column1 = appendToColumn(column1, a);
-           column2 = appendToColumn(column2, b);
-           column3 = appendToColumn(column3, c);
-         }
-       }
+           if (parts1.length == 3 && parts2.length == 3 && parts3.length == 3) {
+             if (isTriangle(Integer.parseInt(parts1[0]), Integer.parseInt(parts2[0]), Integer.parseInt(parts3[0]))) count++;
+             if (isTriangle(Integer.parseInt(parts1[1]), Integer.parseInt(parts2[1]), Integer.parseInt(parts3[1]))) count++;
+             if (isTriangle(Integer.parseInt(parts1[2]), Integer.parseInt(parts2[2]), Integer.parseInt(parts3[2]))) count++;
+            }
+            lineIndex = 0;
+          }
+        }
 
-       for (int i = 0; i < column1.length; i++) {
-         int[] sides = new int[]{column1[i], column2[i], column3[i]};
-         if (isTriangle(sides)) {
-           count++;
-         }
-       }
-
-       input.close();
+        input.close();
      } catch (FileNotFoundException e) {
        System.out.println("File not found: " + filename);
        return 0;
@@ -84,29 +78,9 @@ public class TriangleTester {
      return count;
    }
 
-   public static int[] appendToColumn(int[] column, int value) {
-     int[] newColumn = new int[column.length + 1];
-     for (int i = 0; i < column.length; i++) {
-       newColumn[i] = column[i];
-     }
-     newColumn[column.length] = value;
-     return newColumn;
-   }
-
 
 
   public static void main(String[] args){
-    int[] teststuff = new int[]{5, 10, 25};
-    System.out.println(Arrays.toString(teststuff) + " (should be FALSE): " + isTriangle(teststuff));
-    teststuff = new int[]{5, 100, 5};
-    System.out.println(Arrays.toString(teststuff) + " (should be FALSE): " + isTriangle(teststuff));
-    teststuff = new int[]{5, 5, 2};
-    System.out.println(Arrays.toString(teststuff) + " (should be TRUE): " + isTriangle(teststuff));
-    teststuff = new int[]{1, 2, 3};
-    System.out.println(Arrays.toString(teststuff) + " (should be FALSE): " + isTriangle(teststuff));
-    teststuff = new int[]{3, 2, 3};
-    System.out.println(Arrays.toString(teststuff) + " (should be TRUE): " + isTriangle(teststuff));
-
     String filename = "inputTri.txt";
     int validTrianglesA = countTrianglesA(filename);
     System.out.println("Valid triangles in Part A: " + validTrianglesA);
@@ -114,5 +88,4 @@ public class TriangleTester {
     int validTrianglesB = countTrianglesB(filename);
     System.out.println("Valid triangles in Part B: " + validTrianglesB);
   }
-
 }
