9830ed91f94d1081c55e27548041f4d18277d6c0
anjali814 <abechu60@stuy.edu>
2024-11-05 19:58:34 -0500

    added part b

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriangleTester {
-
+// Part A
   public static int countTrianglesA(String filename) {
     int count = 0;
     try {
@@ -40,6 +40,47 @@ public class TriangleTester {
     return true;
   }
 
+// Part B
+   public static int countTrianglesB(String filename) {
+     int count = 0;
+     int[] column1 = new int[0];
+     int[] column2 = new int[0];
+     int[] column3 = new int[0];
+
+     try {
+       File file = new File(filename);
+       Scanner input = new Scanner(file);
+
+       while(input.hasNextLine()){
+         String line = input.nextLine();
+         String[] parts = line.split(" ");
+
+         if (parts.length == 3) {
+           int a = Integer.parseInt(parts[0]);
+           int b = Integer.parseInt(parts[1]);
+           int c = Integer.parseInt(parts[2]);
+
+           column1 = appendToColumn(column1, a);
+           column2 = appendToColumn(column2, b);
+           column3 = appendToColumn(column3, c);
+         }
+
+         for (int i = 0; i < column1.length; i++) {
+           int[] sides = new int[]{column1[i], column2[i], column3[i]};
+           if (isTriangle(sides)) {
+             count++;
+           }
+         }
+
+       input.close();
+     } catch (FileNotFoundException e) {
+       System.out.println("File not found: " + filename);
+       return 0;
+     }
+     return count;
+   }
+
+
   public static void main(String[] args){
     int[] teststuff = new int[]{5, 10, 25};
     System.out.println(teststuff + " (should be FALSE): " + isTriangle(teststuff));
@@ -52,7 +93,7 @@ public class TriangleTester {
     teststuff = new int[]{3, 2, 3};
     System.out.println(teststuff + " (should be TRUE): " + isTriangle(teststuff));
 
-    String filename = "inputTri.txt"; 
+    String filename = "inputTri.txt";
     int validTriangles = countTrianglesA(filename);
     System.out.println("Valid triangles in Part A: " + validTriangles);
       }
