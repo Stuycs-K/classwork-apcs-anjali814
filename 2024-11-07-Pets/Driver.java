daf7e5b69b656c11b905430ee97f64a55b201a16
anjali814 <abechu60@stuy.edu>
2024-11-07 10:59:30 -0500

    Overrode speak() in Bird and wrote comment in Driver

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+/* When we overrode getName to print "The Mighty NAME" instead of
+   just "NAME" for birds, calling speak() on a bird object caused the System
+   to print the "The Mighty NAME" instead of "NAME".*/
 public class Driver {
   public static void main(String[] args) {
     Animal doggo = new Animal("bark", 2, "Charlie");
