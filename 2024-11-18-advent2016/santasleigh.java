fc48fa3c3e68d8cdfd5a3a55358bd91331321f00
anjali814 <abechu60@stuy.edu>
2024-11-19 04:46:44 -0500

    working on problem 1

--- 2024-11-18-advent2016/santasleigh.java
+++ 2024-11-18-advent2016/santasleigh.java
@@ -1,13 +1,18 @@
 public class santasleigh{
   public static int easterBunnyHQ(String filename){
-    int [] xcoordinate = {-1, 1, 0}
-    int [] ycoordinate = {0, 1, -1}
+    int [] xcoordinate = {-1, 0, 1, 0}
+    int [] ycoordinate = {0, 1, 0, -1}
 
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
 
-    String direction =
+      String direction = input.nextLine();
 
-    for (i = 0; i < direction.length; i++){
-      
+      for (i = 0; i < direction.length; i++){
+        char turn = instructions.charAt(i);
+        int steps = Integer.parseInt(instructions.substring(i+1));
+
     }
 
 
