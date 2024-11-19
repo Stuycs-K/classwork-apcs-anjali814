22951acce3a966f0d060df301882569c727f5005
anjali814 <abechu60@stuy.edu>
2024-11-19 09:55:02 -0600

    added the test and finished my if statement along with the exception- the code passed

--- 2024-11-18-advent2016/bathroomsecurity.java
+++ 2024-11-18-advent2016/bathroomsecurity.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class bathroomsecurity{
   public static String bathroomCode(String filename){
     int x = 1;
@@ -10,32 +14,40 @@ public class bathroomsecurity{
       Scanner input = new Scanner(file);
 
       while (input.hasNextLine()){
-      String direction = input.nextLine();
-      for (int i = 0; i < direction.length(); i++) {
-        char move = direction.charAt(i);
-        int newX = x;
-        int newY = y;
+        String direction = input.nextLine();
+        for (int i = 0; i < direction.length(); i++){
+          char move = direction.charAt(i);
+          int newX = x;
+          int newY = y;
 
-        if (move == 'U') {
-          newX = x - 1;
-        } else if (move == 'D') {
+          if (move == 'U') {
+            newX = x - 1;
+          } else if (move == 'D') {
+            newX = x + 1;
+          } else if (move == 'L') {
+            newY = y - 1;
+          } else if (move == 'R') {
+            newY = y + 1;
+          }
 
-
-
-
-
-
-        if (newX >= 0 && newX < 3 && newY >= 0 && newY < 3) {
-          x = newX;
-          y = newY;
+          if (newX >= 0 && newX < 3 && newY >= 0 && newY < 3) {
+            x = newX;
+            y = newY;
+          }
         }
+        code += (3 * x + y + 1);
       }
-      code += (3 * x + y + 1);
+      input.close();
+    } catch (FileNotFoundException e){
+      System.out.println("File not found: " + filename);
+      return null;
     }
-  } catch
-}
-return code;
-}
-
+    return code;
   }
+
+  public static void main(String[] args) {
+    String filename = "inputCode.txt";
+    System.out.println(bathroomCode(filename));
+  }
+
 }
