8e543344897773d020e9ff0f83115f432baf4473
anjali814 <abechu60@stuy.edu>
2024-11-19 04:53:38 -0500

    added most of the code that works (i think) including a for loop and an if statement

--- 2024-11-18-advent2016/santasleigh.java
+++ 2024-11-18-advent2016/santasleigh.java
@@ -3,6 +3,8 @@ public class santasleigh{
     int [] xcoordinate = {-1, 0, 1, 0}
     int [] ycoordinate = {0, 1, 0, -1}
 
+    int currentDirection = 0;
+
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
@@ -12,9 +14,18 @@ public class santasleigh{
       for (i = 0; i < direction.length; i++){
         char turn = instructions.charAt(i);
         int steps = Integer.parseInt(instructions.substring(i+1));
-
-    }
-
-
+        if (turn == 'L') {
+          currentDirection = (currentDirection + 3) % 4;
+        } else if (turn == 'R') {
+          currentDirection = (currentDirection + 1) % 4;
+        }
+        x += xcoordinate[currentDirection] * steps;
+        y += ycoordinate[currentDirection] * steps;
+      }
+      input.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found: " + filename);
+      return 0;
+    }  
   }
 }
