ef20915ff9bc133b4efbddd8c9e2482981c52177
anjali814 <abechu60@stuy.edu>
2024-11-20 09:39:41 -0500

    added a for loop to iterate over each column, and a for loop to count the frequency of the letters and a for loop to find the letter that appears the most

--- 2024-11-18-advent2016/santacommunicator.java
+++ 2024-11-18-advent2016/santacommunicator.java
@@ -19,6 +19,26 @@ public class santacommunicator{
 
       int length = lines.get(0).length();
 
+      for (int i = 0; i < length; i++) {
+        int[] freq = new int[26];
+
+        for (int j = 0; j < lines.size(); j++) {
+          char c = lines.get(j).charAt(i);
+          freq[c - 'a']++;
+
+
+        int maxFreq = 0;
+        char mostFrequentChar = 'a';
+        for (int k = 0; k < 25; k++) {
+          if (freq[k] > maxFreq) {
+            maxFreq = freq[k];
+            mostFrequentChar = (char) (k + 'a');
+          }
+        }
+      }
+        message += mostFrequentChar;
+
     }
+    return password;
   }
 }
