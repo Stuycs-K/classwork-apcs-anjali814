691b6a349996c85747a68889595a3993a92d2783
anjali814 <abechu60@stuy.edu>
2024-12-03 10:14:02 -0600

    added strings for the colors

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,15 @@
+public class ColorDemo{
+  public static final String RESET = "\u001b[0m";
+  public static final String WHITE = "\u001b[37m";
+  public static final String BLACK = "\u001b[30m";
+  public static final String RED = "\u001b[31m";
+  public static final String YELLOW = "\u001b[33m";
+  public static final String GREEN = "\u001b[32m";
+  public static final String CYAN = "\u001b[36m";
+  public static final String BLUE = "\u001b[34m";
+  public static final String MAGENTA = "\u001b[35m";
+
+  public static void main (String[] args){
+    System.out.println("this text is red" + RED + " red" + RESET);
+  }
+}
