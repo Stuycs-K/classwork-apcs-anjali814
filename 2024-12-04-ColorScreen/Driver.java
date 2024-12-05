f62546116b87e8d0177385890c438c1a2d7940f4
anjali814 <abechu60@stuy.edu>
2024-12-04 23:31:29 -0500

    added random array and the colors

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,6 +1,6 @@
 import java.util.Random;
-
 public class Driver{
+
   public static void drawBorder() {
     for (int col = 0; col < 80; col++) {
       Text.go(1, col + 1);
@@ -23,9 +23,53 @@ public class Driver{
     }
   }
 
+  public static int[] RandomNumbers() {
+      Random random = new Random();
+      int[] numbers = new int[3];
+      for (int i = 0; i < 3; i++) {
+          numbers[i] = random.nextInt(100);
+      }
+      return numbers;
+  }
+  
+  public static void ColorNumbers(int numbers){
+    if (numbers < 25) {
+      Text.color(Text.BRIGHT, Text.RED);
+    } else if (numbers > 75) {
+      Text.color(Text.BRIGHT, Text.GREEN);
+    } else {
+      Text.color(Text.BRIGHT, Text.WHITE);
+    }
+    System.out.print(String.format("%2d ", numbers));
+  }
+
+  public static void DisplayNumbers(int[] numbers){
+    int first = 1;
+    int second = 79 / 3 + 1;
+    int third = ((2 * 79) / 3 + 1);
+    Text.go(2, first);
+    ColorNumbers(numbers[0]);
+
+    Text.go(2, second);
+    ColorNumbers(numbers[1]);
+
+    Text.go(2, third);
+    ColorNumbers(numbers[2]);
+  }
+
+
+
+
   public static void main(String[] args) {
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
+
     drawBorder();
+    int[] numbers = RandomNumbers();
+    DisplayNumbers(numbers);
+    drawBorder();
+
+    System.out.print(Text.RESET);
+    System.out.print(Text.SHOW_CURSOR);
   }
 }
