5f07d454171da385946ab8ed0d24ddcc2045aadb
anjali814 <abechu60@stuy.edu>
2024-12-08 23:54:01 -0500

    added special attack

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -37,9 +37,9 @@ public class Warrior extends Adventurer{
     return this.getName() + " attacks " + other.getName() + "with a damage of " + damage;
   }
 
-  public String support(Adventurer other);{
+  public String support(Adventurer other){
     int heal = 5;
-    other.applyDamage(-heal);
+    other.setHP(other.getHP() + heal);
     return this.getName() + " heals " + other.getName() + "by" + heal;
   }
 
@@ -50,6 +50,13 @@ public class Warrior extends Adventurer{
   }
 
   public String specialAttack(Adventurer other){
-
+    if (this.getSpecial() >= 20){
+      int damage = 30;
+      other.applyDamage(damage);
+      this.setSpecial(this.getSpecial() - 20);
+      return this.getName() + " specially attacks " + other.getName() + " for " + damage + " damage.";
+    } else {
+      return this.getName() + " rage isn't enough for a special attack.";
+    }
   }
 }
