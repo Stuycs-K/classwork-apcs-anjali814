5d4df8abe308592fb86ff63632b6317df4ffd4ae
anjali814 <abechu60@stuy.edu>
2024-12-09 00:27:43 -0500

    added tests

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,3 +1,20 @@
-public static void main (String [] args){
-  
+public class Driver {
+  public static void main (String [] args){
+    Adventurer warrior1 = new Warrior("Bobby", 50);
+    Adventurer warrior2 = new Warrior("Ana", 60);
+
+    System.out.println(warrior1.getName() + " has " + warrior1.getHP() + " HP.");
+    System.out.println(warrior2.getName() + " has " + warrior2.getHP() + " HP.");
+
+    System.out.println(warrior1.attack(warrior2));
+    System.out.println(warrior2.support(warrior1));
+
+    System.out.println(warrior1.support());
+
+    warrior1.restoreSpecial(20);
+    System.out.println(warrior1.specialAttack(warrior2));
+
+    System.out.println(warrior1.getName() + " has " + warrior1.getHP() + " HP and " + warrior1.getSpecial() + " rage.");
+    System.out.println(warrior2.getName() + " has " + warrior2.getHP() + " HP and " + warrior2.getSpecial() + " rage.");
+  }
 }
