4d79481a7fb77b81d202c734c2a35d70ba9dc899
anjali814 <abechu60@stuy.edu>
2024-12-09 09:59:18 -0600

    the class period has ended and started working on the if loop for if it equals attack/ special etc

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class Game{
   public static void main(String[] args){
 
+    Adventurer enemy = new CodeWarrior ("Kevin", 50, "english");
+    Adventurer player = new Warrior ("Susie", 50);
+
+    Scanner userinput = new Scanner(System.in);
+
+    while (enemy.getHP() > 0 && player.getHP() > 0){
+      System.out.println("Player " + player + " Hp:" + player.getHP() + " Special attack: " + player.getSpecialName());
+      System.out.println("Enemy " + enemy + " Hp: " + enemy.getHP() + "Special attack " + enemy.getSpecialName());
+
+      System.out.println("Enter: (a)ttack / (sp)ecial / (su)pport / quit");
+      if ( == 'a' || 'attack'){
+
+      } 
+    }
   }
 }
